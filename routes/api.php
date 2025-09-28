0d21a3dfdcef942bc85d7e195339153c70bcd9ab
DiegoDexx <diegojspro@gmail.com>
2025-09-28 16:05:08 +0200

    login_route

--- routes/api.php
+++ routes/api.php
@@ -31,10 +31,9 @@ Route::get('project-fields/category/{category}', [ProjectFieldController::class,
 // Endpoint para obtener la solicitud completa de un proyecto
 Route::get('projects/{project}/full', [ProjectController::class, 'showFull']);
 
-Route::middleware(['auth:sanctum'])->group(function () {
+Route::post('/logout', [UserController::class, 'logout']);
 
-    //logout
-    Route::post('/logout', [UserController::class, 'logout']);
+Route::middleware(['auth:sanctum'])->group(function () {
 
     // CRUD de clientes salvo post , por lo cual no hay apiresource
     Route::get('clients', [ClientController::class, 'index']);
